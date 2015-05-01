aecb538151b0436aca9a2419153dfacf3c9d7e74
James Schacht <pascal888@yahoo.com>
2015-05-01 08:06:27 -0400

    Signed-off-by: James Schacht <pascal888@yahoo.com>

--- rankall.R
+++ rankall.R
@@ -6,6 +6,7 @@ if (outcome == "heart attack" || outcome == "heart failure" || outcome == "pneum
     data[,17] <- as.numeric(data[,17])
     data[,23] <- as.numeric(data[,23])
     x <- na.omit(data[, c(2, 7, 11, 17, 23)])
+
     unique_States <- sort(unique(data$State))
     for (i in 1:length(unique_States)) {
          subState <- split(x, x$State)
@@ -16,13 +17,11 @@ if (outcome == "heart attack" || outcome == "heart failure" || outcome == "pneum
         for (i in 1:length(unique_States)) {
             subState[[i]] <- subState[[i]][order(subState[[i]]$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack,subState[[i]]$Hospital.Name, subState[[i]]$State),]
         }
-        final <- data.frame(hospital=character(),state=character(),stringsAsFactors=FALSE)
-        for (i in 1: length(unique_States)) {
-#             if (num == "worst") {
-#                 subState[[i]] <- subState[[i]][order(subState[[i]]$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack,subState[[i]]$Hospital.Name, subState[[i]]$State,decreasing=TRUE)]
-#         num <- 1
-#             }
-        final[i,] <- subState[[i]][num[i],1:2]
+       final <- data.frame(hospital=character(),state=character(),stringsAsFactors=FALSE)
+    for (i in 1: length(unique_States)) {
+
+        final[i,] <- subState[[i]][1,]
+ #       print(subState[[i]][1,])
     }
     return(final)
     }
