312858f3a46920885435d6626bbb90e0b6df0c71
James Schacht <pascal888@yahoo.com>
2015-05-01 14:28:31 -0400

    Bingo!   with help from Karl Schultz!
    Signed-off-by: James Schacht <pascal888@yahoo.com>

--- rankall.R
+++ rankall.R
@@ -5,7 +5,7 @@ rankall <- function(outcome, num = "best") {
         data[,11] <- as.numeric(data[,11])
         data[,17] <- as.numeric(data[,17])
         data[,23] <- as.numeric(data[,23])
-        x <- na.omit(data[, c(2, 7, 11, 17, 23)])
+        x <- data[, c(2, 7, 11, 17, 23)]
 
         unique_States <- sort(unique(data$State))
         for (i in 1:length(unique_States)) {
@@ -15,12 +15,12 @@ rankall <- function(outcome, num = "best") {
         ## Begin the ranking by outcome
         if (outcome == "heart attack") {
             for (i in 1:length(unique_States)) {
-                subState[[i]] <- na.omit(subState[[i]][, c(1,2,3,4,5)])
+                subState[[i]] <- subState[[i]][, c(1,2,3,4,5)]
                 subState[[i]] <- subState[[i]][order(subState[[i]]$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack,subState[[i]]$Hospital.Name, subState[[i]]$State),]
            }
         final <- data.frame(hospital=character(),state=character(),stringsAsFactors=FALSE)
         for (i in 1: length(unique_States)) {
-            subState[[i]] <- na.omit(subState[[i]][, c(1,2,3,4,5)])
+            subState[[i]] <- subState[[i]][, c(1,2,3,4,5)]
             if (num == "worst") {
                 subState[[i]] <- subState[[i]][order(-subState[[i]]$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack,subState[[i]]$Hospital.Name, subState[[i]]$State),]
             }
@@ -31,12 +31,12 @@ rankall <- function(outcome, num = "best") {
         ## end outcome heart attack
         if (outcome == "heart failure") {
             for (i in 1:length(unique_States)) {
-                subState[[i]] <- na.omit(subState[[i]][, c(1,2,3,4,5)])
+                subState[[i]] <- subState[[i]][, c(1,2,3,4,5)]
                 subState[[i]] <- subState[[i]][order(subState[[i]]$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure,subState[[i]]$Hospital.Name, subState[[i]]$State),]
             }
             final <- data.frame(hospital=character(),state=character(),stringsAsFactors=FALSE)
             for (i in 1: length(unique_States)) {
-                subState[[i]] <- na.omit(subState[[i]][, c(1,2,3,4,5)])
+                subState[[i]] <- subState[[i]][, c(1,2,3,4,5)]
                 if (num == "worst") {
                     subState[[i]] <- subState[[i]][order(-subState[[i]]$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure,subState[[i]]$Hospital.Name, subState[[i]]$State),]
                 }
@@ -47,12 +47,12 @@ rankall <- function(outcome, num = "best") {
         ## end outcome heart failure
         if (outcome == "pneumonia") {
             for (i in 1:length(unique_States)) {
-                subState[[i]] <- na.omit(subState[[i]][, c(1,2,3,4,5)])
+                subState[[i]] <- subState[[i]][, c(1,2,3,4,5)]
                 subState[[i]] <- subState[[i]][order(subState[[i]]$Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia,subState[[i]]$Hospital.Name, subState[[i]]$State),]
             }
             final <- data.frame(hospital=character(),state=character(),stringsAsFactors=FALSE)
             for (i in 1: length(unique_States)) {
-                subState[[i]] <- na.omit(subState[[i]][, c(1,2,3,4,5)])
+                subState[[i]] <- subState[[i]][, c(1,2,3,4,5)]
                 if (num == "worst") {
                     subState[[i]] <- subState[[i]][order(-subState[[i]]$Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia,subState[[i]]$Hospital.Name, subState[[i]]$State),]
                 }
